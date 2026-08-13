0883c05474f7554b40a50cc4db4a6c138c0ee20c
Muchprow Games <dlyaigr2282011@gmail.com>
2026-08-13 18:07:32 +0300

    Modify setup.py for Python performance extension
    Updated the description to reflect Python performance.

--- setup.py
+++ setup.py
@@ -5,6 +5,6 @@ module = Extension('fast_crow', sources=['fast_crow.c'])
 setup(
     name='fast_crow',
     version='1.0',
-    description='Native C Extension for Crow Compiler Performance',
+    description='Native C Extension for Python Performance',
     ext_modules=[module]
 )
