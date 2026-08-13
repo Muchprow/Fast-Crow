0997a8c708afa18f02029aaf3367dcaa2720705f
Muchprow Games <dlyaigr2282011@gmail.com>
2026-08-13 18:06:38 +0300

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, Extension
+
+module = Extension('fast_crow', sources=['fast_crow.c'])
+
+setup(
+    name='fast_crow',
+    version='1.0',
+    description='Native C Extension for Crow Compiler Performance',
+    ext_modules=[module]
+)
