260807434e1f2dfc455599ca9ddb0da14d28f345
Muchprow Games <dlyaigr2282011@gmail.com>
2026-08-13 18:11:21 +0300

    Revise setup.py for fast_crow package
    Updated package version and added long description from README.

--- setup.py
+++ setup.py
@@ -1,10 +1,21 @@
 from setuptools import setup, Extension
 
-module = Extension('fast_crow', sources=['fast_crow.c'])
+fast_crow_module = Extension(
+    'fast_crow',          
+    sources=['fast_crow.c'] 
+)
+
+with open('README.md', 'r', encoding='utf-8') as f:
+    long_description = f.read()
 
 setup(
-    name='fast_crow',
-    version='1.0',
-    description='Native C Extension for Python Performance',
-    ext_modules=[module]
+    name='fast_crow',                   
+    version='0.1.0',                    
+    author='Muchprow',
+    description='A fast string cleaner library written in C for Python',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/Muchprow/Fast-Crow',
+    ext_modules=[fast_crow_module],     
+    python_requires='>=3.6',
 )
